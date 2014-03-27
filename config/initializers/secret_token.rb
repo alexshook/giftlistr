e6857563853f2f1f9149c0e02019ee69c73cd1d5
alexshook <alexandra.shook@gmail.com>
2014-03-27 16:08:00 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Giftlistr::Application.config.secret_key_base = '77189ee92cebda11e54c2ab744d919856832b0dedda329f5fac0ad956620d4165c069d2b6ddb48605a422e3b8197bdf49aee51c1f5a1ab215ca688c6903f1fa2'
