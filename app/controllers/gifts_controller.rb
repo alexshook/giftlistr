d4c7c41c8bc612f91d17bd1d3401c029324b492a
alexshook <alexandra.shook@gmail.com>
2014-03-31 14:29:21 -0400

    adding gift search form

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,8 +1,20 @@
 class GiftsController < ApplicationController
   before_action :authenticate, except: [:index, :show]
-  binding.pry
+
   def index
-    @gifts = HTTParty.get("https://openapi.etsy.com/v2/listings/active?api_key="+"#{ENV['ETSY_KEYSTRING']}")["results"]
+    # @gifts = HTTParty.get("https://openapi.etsy.com/v2/listings/active?api_key="+"#{ENV['ETSY_KEYSTRING']}"+"&fields=title,price,tags,url,category_path&limit=5&includes=MainImage")["results"]
+
+    # check this
+    # @etsy_url = "https://openapi.etsy.com/v2/listings/active?api_key="+"#{ENV['ETSY_KEYSTRING']}"+"&fields=title,price,tags,url,category_path&limit=5&includes=MainImage&search?q="+@user_input
+   user_input = []
+   user_input.push(params[:q1], params[:q2], params[:q3], params[:q4])
+   query = user_input.join
+    @etsy_url = "https://openapi.etsy.com/v2/listings/active?api_key="+"#{ENV['ETSY_KEYSTRING']}"+"&fields=title,price,tags,url,category_path&limit=5&includes=MainImage&search?q="+"#{query}"
+
+    @personalized_gifts = HTTParty.get(@etsy_url)["results"]
+  end
+
+  def show
   end
 
 end
--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,5 +1,5 @@
 class Gift < ActiveRecord::Base
   has_and_belongs_to_many :giftlists
-
   belongs_to :user
+
 end
