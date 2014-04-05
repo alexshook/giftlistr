30c9562a3c1cd191808d127bb393edf5543143b7
alexshook <alexandra.shook@gmail.com>
2014-04-05 17:10:27 -0400

    overriding devise registrations controller and modifying routes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  def edit
+    # @user = User.find params[:id]
+  end
 
+  private
+  def user_params
+    params.require(:user).permit(:avatar)
+  end
 end
