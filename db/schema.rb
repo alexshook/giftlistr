64215ec4b987f95855fd4394985b13ee90704a58
alexshook <alexandra.shook@gmail.com>
2014-04-05 22:59:41 -0400

    cleaning up schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140404211620) do
+ActiveRecord::Schema.define(version: 20140406025651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,10 +41,7 @@ ActiveRecord::Schema.define(version: 20140404211620) do
     t.string  "name"
     t.integer "price"
     t.string  "category"
-    t.integer "tag_id"
   end
-
-  add_index "gifts", ["tag_id"], name: "index_gifts_on_tag_id", using: :btree
 
   create_table "gifts_tags", id: false, force: true do |t|
     t.integer "gift_id"
