0af48ee8425ed5303c9065731ab12ca9f7116bf1
alexshook <alexandra.shook@gmail.com>
2014-04-09 19:38:48 -0400

    can almost sign in with facebook

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     before_action :authenticate_user!, except: [:index]
 
   def update
-    #FIXME :avatar is not passing through
+    #FIXME :avatar is throwing the sorry, something went wrong notice. Password can be updated without a problem. Bucket name is correct. Image url throws 404 in console and s3 bucket has no file or images.
     if @user.update user_params
       flash[:notice] = "Your profile has been updated! "
       redirect_to @user
