3efe811b8c8f77afb0603712f8594e4ab35ab8ee
alexshook <alexandra.shook@gmail.com>
2014-07-03 19:10:20 -0400

    modifying links in navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-
-# Examples:
-
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 # users = User.create([
 # {
 #   email: Faker::Internet.email,
@@ -45,54 +37,54 @@
 #    ])
 
 
-gifts = Gift.create([
-  {
-  name: Faker::Product.product_name,
-  price: 10,
-},
-  {
-  name: Faker::Product.product_name,
-  price: 20,
-},   {
-  name: Faker::Product.product_name,
-  price: 30,
-},
-  {
-  name: Faker::Product.product_name,
-  price: 10,
-},
-  {
-  name: Faker::Product.product_name,
-  price: 20,
-},   {
-  name: Faker::Product.product_name,
-  price: 30,
-},
- ])
+# gifts = Gift.create([
+#   {
+#   name: Faker::Product.product_name,
+#   price: 10,
+# },
+#   {
+#   name: Faker::Product.product_name,
+#   price: 20,
+# },   {
+#   name: Faker::Product.product_name,
+#   price: 30,
+# },
+#   {
+#   name: Faker::Product.product_name,
+#   price: 10,
+# },
+#   {
+#   name: Faker::Product.product_name,
+#   price: 20,
+# },   {
+#   name: Faker::Product.product_name,
+#   price: 30,
+# },
+#  ])
 
-giftlists = Giftlist.create([
-  {
-  name: "Giftlist 1",
-  note: "birthday present ideas",
-  giftee: Faker::Name.first_name,
-  user_id: 1
-  },
-  {
-  name: "Giftlist 2",
-  note: "housewarming present ideas",
-  giftee: Faker::Name.first_name,
-  user_id: 1
-  },
-    {
-  name: "Giftlist 3",
-  note: "graduation present ideas",
-  giftee: Faker::Name.first_name,
-  user_id: 2
-  },
-    {
-  name: "Giftlist 4",
-  note: "birthday present ideas",
-  giftee: Faker::Name.first_name,
-  user_id: 2
-  },
-])
+# giftlists = Giftlist.create([
+#   {
+#   name: "Giftlist 1",
+#   note: "birthday present ideas",
+#   giftee: Faker::Name.first_name,
+#   user_id: 1
+#   },
+#   {
+#   name: "Giftlist 2",
+#   note: "housewarming present ideas",
+#   giftee: Faker::Name.first_name,
+#   user_id: 1
+#   },
+#     {
+#   name: "Giftlist 3",
+#   note: "graduation present ideas",
+#   giftee: Faker::Name.first_name,
+#   user_id: 2
+#   },
+#     {
+#   name: "Giftlist 4",
+#   note: "birthday present ideas",
+#   giftee: Faker::Name.first_name,
+#   user_id: 2
+#   },
+# ])
